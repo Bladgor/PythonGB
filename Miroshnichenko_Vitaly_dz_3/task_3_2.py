aa3dff78afb511347fe4bfda4b3812c1b811050b
Bladgor <vint_666@bk.ru>
2022-01-03 12:11:04 +0300

    lesson_3

---
+++ Miroshnichenko_Vitaly_dz_3/task_3_2.py
@@ -0,0 +1,27 @@
+
+
+def num_translate(num, flag=False):
+    if num[0].isupper():
+        flag = True
+    if num.lower() in TRANSLATE_DICT:
+        if flag:
+            return TRANSLATE_DICT[num.lower()].title()
+        return TRANSLATE_DICT[num]
+
+
+TRANSLATE_DICT = {
+    'one': 'один',
+    'two': 'два',
+    'three': 'три',
+    'four': 'четыре',
+    'five': 'пять',
+    'six': 'шесть',
+    'seven': 'семь',
+    'eight': 'восемь',
+    'nine': 'девять',
+    'ten': 'десять'
+}
+
+number = input('Введите число на английском: ')
+
+print(num_translate(number))
