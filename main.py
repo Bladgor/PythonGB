a6ec0f10b54283aa87a793142d3d687db751ce87
Bladgor <vint_666@bk.ru>
2022-05-31 21:55:08 +0300

    скопировал шаблон бота

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
-import os
-from dotenv import load_dotenv, find_dotenv
+from loader import bot
+import handlers
+from utils.set_bot_commands import set_default_commands
 
-if not find_dotenv():
-    exit('Переменные окружения не загружены т.к отсутствует файл .env')
-else:
-    load_dotenv()
-
-BOT_TOKEN = os.getenv('BOT_TOKEN')
-RAPID_API_KEY = os.getenv('RAPID_API_KEY')
-DEFAULT_COMMANDS = (
-    ('start', "Запустить бота"),
-    ('help', "Вывести справку")
-)
-
+if __name__ == '__main__':
+    set_default_commands(bot)
+    bot.infinity_polling()
